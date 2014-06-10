a8431d82f6a09a8c589ec1b27dbcf7b76c90b1d9
Eugene Hsu <dreammacer.yo@gmail.com>
2014-06-11 00:17:54 +0800

    Add Facebook Login

--- app.js
+++ app.js
@@ -10,11 +10,11 @@ var flash = require('connect-flash');
 var mongoose = require('mongoose');
 var passport = require('passport');
 
-// require('./config/db'); // TODO [DB] : Connect to database
-// require('./config/passport'); // TODO [FB] : Passport configuration
+require('./config/db'); // TODO [DB] : Connect to database
+require('./config/passport'); // TODO [FB] : Passport configuration
 
 var app = express();
-// var Vote = mongoose.model('Vote'); // TODO [DB] : Get Vote model
+var Vote = mongoose.model('Vote'); // TODO [DB] : Get Vote model
 
 // all environments
 app.set('port', process.env.PORT || 3000);
@@ -58,22 +58,22 @@ app.post('/vote', function(req, res, next){
   // Directly invoke the passport authenticate middleware.
   // Ref: http://passportjs.org/guide/authenticate/
   //
-  // passport.authenticate('facebook')(req, res, next);
+  passport.authenticate('facebook')(req, res, next);
 });
 
 // TODO [FB]: Facebook callback handler
 // Ref: https://github.com/jaredhanson/passport-facebook/blob/master/examples/login/app.js#L100
 //
-// app.get('/fbcb', passport.authenticate('facebook', {
-//   successRedirect:'/result',
-//   failureRedirect: '/'
-// }));
+app.get('/fbcb', passport.authenticate('facebook', {
+  successRedirect:'/result',
+  failureRedirect: '/'
+}));
 
 app.get('/result', function(req, res){
 
   var vote = req.session.vote, // The voted item (0~6)
       fbid = "" + Math.random();    // Facebook ID. (Fake)
-      // fbid = req.user && req.user.id; // TODO [FB]: Get user from req.user
+      fbid = req.user && req.user.id; // TODO [FB]: Get user from req.user
 
   // Delete the stored session.
   //
@@ -94,20 +94,20 @@ app.get('/result', function(req, res){
   */
 
   //
-  // var vote = new Vote({vote: vote, fbid: fbid});
-  // vote.save(function(err, newVote){
-  //   if( err ){
-  //     req.flash('info', "你已經投過票囉！");
-  //     return res.redirect('/');
-  //   }
+  var vote = new Vote({vote: vote, fbid: fbid});
+  vote.save(function(err, newVote){
+    if( err ){
+      req.flash('info', "你已經投過票囉！");
+      return res.redirect('/');
+    }
   //
   //   ... ...
   //
        res.render('result', {
          votes: [18.1, 12.5, 42.44445, 21.3, 1.3, 2.5, 1.85555] // Percentages
        });
-  //
-  // });
+
+  });
 
 });
 
